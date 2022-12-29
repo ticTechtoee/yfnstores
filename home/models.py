befce20ef352a55d5b0bdec8d568a067935080c1
Farhan Basheer <farhanfb6@outlook.com>
2022-12-29 12:58:53 +0500

    Theme Cleaned Up & Model Created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length=100)
+    description =  models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Images(models.Model):
+    image = models.ImageField()
+    product = models.ForeignKey("Products", on_delete=models.PROTECT)
