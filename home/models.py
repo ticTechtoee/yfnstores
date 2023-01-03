28af9f743010388bb5813d74ab3500b29cf36c68
Farhan Basheer <farhanfb6@outlook.com>
2023-01-03 22:07:10 +0500

    Fixed the default image issue

--- home/models.py
+++ home/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Products(models.Model):
     title = models.CharField(max_length=100)
     description =  models.TextField()
-    image1 = models.ImageField(upload_to='post_images/',blank=True, null=True)
-    image2 = models.ImageField(upload_to='post_images/',blank=True, null=True)
-    image3 = models.ImageField(upload_to='post_images/',blank=True, null=True)
+    image1 = models.ImageField(upload_to='post_images/', default='post_images/default.jpg',blank=True, null=True)
+    image2 = models.ImageField(upload_to='post_images/', default='post_images/default.jpg',blank=True, null=True)
+    image3 = models.ImageField(upload_to='post_images/', default='post_images/default.jpg',blank=True, null=True)
     cardLink = models.URLField(max_length=500)
     def __str__(self):
         return self.title
