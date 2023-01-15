ec0d8c2b400f3c60b1847cc21281b4f7b9ac88ab
Farhan Basheer <farhanfb6@outlook.com>
2023-01-15 19:19:34 +0500

    Added the Email Module

--- home/views.py
+++ home/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from .models import Products
+from django.conf import settings
+from django.http import HttpResponse
+from django.core.mail import EmailMultiAlternatives
 
 def index(request):
     prod = Products.objects.all()
     context = {'Products':prod}
+    if request.method == "POST":
+        custName = request.POST.get("name")
+        custEmail = request.POST.get("email")
+        custsubject = "Inquirey About " + request.POST.get("subject")
+        custBody = request.POST.get("message")
+        subject = custsubject
+        email_body = "Email From Name : "+ custName + " Email Address : " + custEmail + " Inquiry About : "+ custBody
+        
+        from_email = settings.EMAIL_HOST_USER
+        to_email = ("yfnstores@gmail.com", "")
+        message = EmailMultiAlternatives(
+            subject=subject, body=email_body, from_email=from_email, to=to_email
+        )
+        message.content_subtype = "html"
+        message.send()
     return render(request, 'home/index.html', context)
 
 def prod_details(request,pk):
--- yfnstores/settings.py
+++ yfnstores/settings.py
@@ -205,4 +205,13 @@ CKEDITOR_5_CONFIGS = {
     }
 }
 
+DEFAULT_FROM_EMAIL = 'info.yfnstores@gmail.com'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'info.yfnstores@gmail.com'
+EMAIL_HOST_PASSWORD = 'amhualkfiqafovaq'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
