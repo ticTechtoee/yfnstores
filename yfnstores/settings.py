9976aa927fd60124428673d59ac49968b713212b
Farhan Basheer <bfarhan8@gmail.com>
2023-01-24 09:55:55 +0500

    Removed Password

--- yfnstores/settings.py
+++ yfnstores/settings.py
@@ -133,7 +133,7 @@ DEFAULT_FROM_EMAIL = 'info.yfnstores@gmail.com'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'info.yfnstores@gmail.com'
-EMAIL_HOST_PASSWORD = 'amhualkfiqafovaq'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
